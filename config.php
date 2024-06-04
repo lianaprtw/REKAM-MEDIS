dd70747b0b054f0eb535a34e206615db4e1af4ee
Enno Lerebulan <148016035+Lerebulanenno@users.noreply.github.com>
2024-06-04 18:44:20 +0800

    tambah_pasien

--- config.php
+++ config.php
@@ -8,11 +8,11 @@ $dbname = 'rekammedis';
 $koneksi = mysqli_connect($host, $user, $pass, $dbname);
 
 // kode untuk mengecek koneksi
-if (!$koneksi) {
-    die('koneksi gagal');
-} else {
-    echo "Koneksi berhasil";
-}   
+// if (!$koneksi) {
+//     die('koneksi gagal');
+// } else {
+//     echo "Koneksi berhasil";
+// }   
 
 $main_url = "http://localhost/REKAM-MEDIS/"
 ?>
